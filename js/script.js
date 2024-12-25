1a8c9292efe902a4bd864b9e483cecb43aa5bd3c
Cat Noir <imomatdinovdavronbek@gmail.com>
2024-12-25 19:37:59 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function checkPassword() {
+    const correctPassword = "THE UNIVERSE GROUP"; // To'g'ri parol
+    const userPassword = prompt("Parolni kiriting:");
+
+    if (userPassword === correctPassword) {
+      alert("Qoyil, yozgan paroling to'g'ri");
+      window.location.href = "matem.html"; // Yo'naltiriladigan sahifa
+      } else {
+          alert("Parol noto'g'ri! Qaytadan urinib ko'ring.");
+            }
+        }
